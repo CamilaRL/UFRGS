e11f8e53d23fab176f162df8e8e56398922c922b
CamilaRL <camila.raupp.luz7@gmail.com>
2023-07-25 15:53:21 -0300

    bc junto do for

--- RandomWalker/SelfAvoiding/selfavoid.c
+++ RandomWalker/SelfAvoiding/selfavoid.c
@@ -7,8 +7,10 @@
 #define N 1000 // numero de passos
 
 int x = 0, y = 0; // [x, y]
+int s[L*L] = {0}; // rede quadrada. onde o caminhante estiver é 1
 
-void walker(FILE *file);
+void Walker(FILE *file);
+int Gameover(int w);
 
 void main(void){
 
@@ -25,19 +27,18 @@ void main(void){
 		fprintf(saida, "#Amostra %ld L %d Nmax %d\n", seed, L, N);
 	    fprintf(saida, "#N w x y dr2\n");
 
-		walker(saida);
+		Walker(saida);
 	}
 
     fclose(saida);  
 }
 
-void walker(FILE *file){
+void Walker(FILE *file){
 
-    int w = 50;
+    int w = 30;
     int waux = w;
 	int c;
     double dr2 = 0.0;
-    int s[L*L] = {0}; // rede quadrada. onde o caminhante estiver é 1
     int bc = 0; // 0 se não cruzou os limites da caixa | 1 se cruzou os limites da caixa
     int gameover = 0; // 0 a cobrinha n travou | 1 a cobrinha travou
     int n = 0;
@@ -46,34 +47,30 @@ void walker(FILE *file){
     y = 0;
 
     do{
-		// Verifica se já passou pelo sítio e se ele tem para onde ir
-		//fprintf(file, "%d %d %d %d w=%d L=%d\n", s[w-1], s[w+1], s[w-L], s[w+L], w, L);
-		
-        if( (s[w-1]+s[w+1]+s[w-L]+s[w+L]) == 4){
-            gameover = 1;
-        }
-        else if(s[waux] == 0){
-			
-			dr2 = pow(x,2) + pow(y,2);
 
-			fprintf(file, "%d %d %d %d %f %d\n", n, w, x, y, dr2, bc);
-            
-			w = waux;
+        // Verifica se já passou pelo sítio
+        if(s[waux] == 0){
+
+            w = waux;
             s[w] = 1;
+            dr2 = pow(x,2) + pow(y,2);
+
+            fprintf(file, "%d %d %d %d %f %d\n", n, w, x, y, dr2, bc);
+
             n++;
         }
-		
+
         c = (int) rand()%4;
-        //fprintf(file, "c=%d gameover=%d %d,%d ",c, gameover, x, y);
+        
         if(c == 0){
             // sobe
-            y = y + 1; //fprintf(file, "sobe\n");
+            y = y + 1;
 
             if(w < L){
             // se estiver na primeira linha
                 waux = w - L + (L*L);
                 bc = 1;
-            }   
+            }
             else{
                 waux = w - L;
                 bc = 0;
@@ -81,7 +78,7 @@ void walker(FILE *file){
         }
         else if(c == 1){
             // desce
-            y = y - 1; //fprintf(file, "desce\n");
+            y = y - 1;
 
             if(w >= (L*L)-L){
                 // se estiver na última linha
@@ -95,7 +92,7 @@ void walker(FILE *file){
         }
         else if(c == 2){
             // direita
-            x = x + 1; //fprintf(file, "direita\n");
+            x = x + 1;
 
             if(w%L == L-1){
                 // se estiver na última coluna
@@ -109,7 +106,7 @@ void walker(FILE *file){
         }
         else if(c == 3){
             // esquerda
-            x = x - 1; //fprintf(file, "esquerda\n");
+            x = x - 1;
 
             if(w%L == 0){
                 // se estiver na primeira coluna
@@ -122,5 +119,49 @@ void walker(FILE *file){
             }
         }
         
+        gameover = Gameover(w);
+        
     }while(n < N && gameover == 0);
+}
+
+int Gameover(int w){
+
+    int c = 0;
+    int sum = 0;
+    
+    // superior
+    if(w < L){
+        sum += s[w - L + (L*L)];
+    }
+    else{
+        sum += s[w-L];
+    }
+    // inferior
+    if(w >= (L*L)-L){
+        sum += s[w%L];
+    }
+    else{
+        sum += s[w+L];
+    }
+    // esquerda
+    if(w%L == 0){
+        sum += s[w + L - 1];
+    }
+    else{
+        sum += s[w-1];
+    }
+    // direita
+    if(w%L == L-1){
+        sum += s[w - L + 1];
+    }
+    else{
+        sum += s[w+L];
+    }
+    
+    
+    if(sum == 4){
+        c = 1;
+    }
+    
+    return c;
 }
